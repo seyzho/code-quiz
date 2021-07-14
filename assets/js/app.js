30c6b2a700f35d037706bbedff8ac4e75117b167
ShereKhan Adams <sherekhanadams@gmail.com>
2021-07-13 21:59:35 -0700

    fixing js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,158 +1,194 @@
+const startButton = document.getElementById('start-btn')
+const questionContainer = document.getElementById('quiz-box')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const nextButton = document.getElementById('next-btn')
+const submitButton = document.getElementById('submit-btn')
+const endForm = document.getElementById('end-form')
 
-let timeEl = document.querySelector("p.time");
-let secondsLeft = 60;
-let introEl = document.querySelector("#intro");
-let questionsEl = document.querySelector("#questions");
-let questionEl = document.querySelector("#question");
-let questionCount = 0;
+var sec
+let shuffledQuestions, currentQuestionIndex
+var score = 0
 
-let correctwrongEl = document.querySelector("#correctwrong");
-let startBtn = document.querySelector("#start");
-let submitScrBtn = document.querySelector("#submit-score");
+startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion()
+})
+submitButton.addEventListener('click', submitQuiz)
 
-let homeBtn = document.querySelector("#home");
-let clearScrBtn = document.querySelector("#clearscores");
-let viewScrBtn = document.querySelector("#view-scores");
-let answerBtn= document.querySelectorAll("button.answerButton")
+function startQuiz() {
+    startButton.classList.add('hide')
+    submitButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainer.classList.remove('hide')
+    endForm.classList.add('hide')
+    startTimer()
+    nextQuestion()
+}
 
-let answer1Btn = document.querySelector("#answer1");
-let answer2Btn = document.querySelector("#answer2");
-let answer3Btn = document.querySelector("#answer3");
-let answer4Btn = document.querySelector("#answer4");
-
-let finalEl = document.querySelector("#final");
-let initialsInput = document.querySelector("#initials");
-let highscoresEl = document.querySelector("#highscores");
-let scoreEl = document.querySelector("#score");
-let scoreListEl = document.querySelector("#score-list");
-let scoreList = [];
-
-let questions = [
-    {
-        question: "does this quiz have to actually be about code",
-        answers: ["1. Yes", "2. No", "3. Probably", "4. this choice is wrong"],
-        correctAnswer: "4" 
-    },
-    {
-        question: "there are __ more questions in this quiz",
-        answers: ["1. 15", "2. 4", "3. 2", "4. 7"],
-        correctAnswer: "3"
-    },
-    {
-        question: "the _______ is the powerhouse of the cell",
-        answers: ["1. brain", "2. mitochondria", "3. stack overflow", "4. fish"],
-        correctAnswer: "2"
-    },
-    {
-        question: "this is the last question on the quiz",
-        answers: ["1. Yes", "2. no there are 3 more questsions", "3. no there are 5 more questions", "4. no there is one more question" ],
-        correctAnswer: "1"
-    }
-
-];
-
-function setTime() {
-    let timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeEl.textContent = `Time:${secondsLeft}`;
-
-        if (secondsLeft === 0 || questionCount === questions.length) {
-            clearInterval(timerInterval);
-            questionsEl.getElementsByClassName.display = "none";
-            finalEL.style.display = "block";
-            scoreEl.textContent = secondsLeft;
+function startTimer() {
+    sec = 180
+    var timer = setInterval(function() {
+        document.getElementById('time').innerHTML=sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
         }
     }, 1000);
 }
 
-function startQuiz() {
-    introEl.style.display = "none";
-    questionsEl.style.display = "block";
-    questionCount = 0;
-
-    setTime();
-    setQuestion(questionCount);
+function nextQuestion() {
+    reset()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function setQuestion(id) {
-    if (id < questions.length) {
-        questionEl.textContent = questions[id].question;
-        answer1Btn.textContent = questions[id].answers[0];
-        answer2Btn.textContent = questions[id].answers[1];
-        answer3Btn.textContent = questions[id].answers[2];
-        answer4Btn.textContent = questions[id].answers[3];
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', yourAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function submitQuiz() {
+    reset()
+    questionContainer.classList.add('hide')
+    endForm.classList.remove('hide')
+    submitButton.classList.add('hide')
+    startButton.classList.remove('hide')
+    startButton.innerText = "Take it again"
+}
+
+function reset() {
+    clearClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
-function checkAnswer(event) {
-    event.preventDefault();
-
-    correctwrongEl.style.display = "block";
-    let p = document.createElement("p");
-    correctwrongEl.appendChild(p);
-
-    setTimeout(function () {
-        p.style.display = "none";
-    }, 500);
-
-if (questions[questionCount].correctAnswer === event.target.value) {
-    p.textContent = "correct";
-} else if (questions[questionCount].correctAnswer !== event.target.value) {
-    secondsLeft = secondsLeft - 10;
-    p.textContent = "incorrect";
-}
-if (questionCount < questions.length) {
-    questionCount++;
-}
-setQuestion(questionCount);
-}
-
-function addScore(event) {
-    event.preventDefault();
-
-    finalEl.style.display = "none";
-    highscoresEl.style.display = "block";
-
-    let init = initialsInput.value.toUpperCase();
-    scoreList.push({ initials: init, score: secondsLeft });
-
-    scoreListEl.innerHTML="";
-    for (let i = 0; i < scoreList.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = `${scoreList[i].initials}; ${scoreList[i].score}`;
-        scoreListEl.append(li);
+function yourAnswer(e) {
+    const selectedAnswer = e.target
+    const correct = selectedAnswer.dataset.correct
+    answerCheck(document.body, correct)
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        submitButton.classList.remove('hide')
     }
-
-    storeScores();
-    displayScores();
 }
 
-function clearScores() {
-    localStorage.clear();
-    scoreListEl.innerHTML="";
+function answerCheck(element, correct) {
+    clearClass(element)
+    if (correct) {
+        score++
+    }
+    else {
+        sec-= 10
+    }
+    Array.from(answerButtonsElement.children).forEach(button => {
+        highlightCorrect(button, button.dataset.correct)
+    })
 }
 
-startBtn.addEventListener("click", startQuiz);
+function highlightCorrect(element, correct) {
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
+    }
+}
 
-answerBtn.forEach(item => {
-    item.addEventListener("click", checkAnswer);
-});
 
-submitScrBtn.addEventListener("cllick", addScore);
+function clearClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
+}
 
-clearScrBtn.addEventListener("click", clearScores);
+const questions = [
+    {
+        question:'does every question need to be about code',
+        answers: [
+            { text: 'yes', correct: false },
+            { text: 'probably', correct: true },
+            { text: 'no', correct: false },
+            { text: '-10', correct: false },
+        ] 
+    
+    },
 
-homeBtn.addEventListener("click", function () {
-    highscoresEl.style.display = "none";
-    introEl.style.display = "block";
-    secondsLeft = 75;
-    timeEl.textContent = `Time:${secondsLeft}`;
-});
+    {
+        question:'the ________ is the powerhouse of the cell',
+        answers: [
+            { text: 'Cytochrome', correct: false },
+            { text: 'mitochondria', correct: true },
+            { text: 'brain', correct: false },
+            { text: 'stack overflow', correct: false },
 
-viewScrBtn.addEventListener("click", function () {
-    if (highscoresEl.style.display === "none") {
-        highscoresEl.style.display = "block";
-    } else if (highscoresEl.style.display === "block") {
-        highscoresEl.style.display = "none";
-    } 
-});
+        ] 
+    
+    },
+
+    {
+        question:"a baker's dozen is __",
+        answers: [
+            { text: '24', correct: false },
+            { text: '10', correct: false },
+            { text: '7', correct: false },
+            { text: '13', correct: true},
+        ] 
+    
+    },
+
+    {
+        question:'challenges are always due on ________',
+        answers: [
+            { text: 'Monday', correct: false },
+            { text: 'Sunday', correct: true },
+            { text: 'Wednesday', correct: false },
+            { text: 'Friday', correct: false },
+        ] 
+    
+    },
+
+    {
+        question:'the script link goes in the ___',
+        answers: [
+            { text: '<head>', correct: false },
+            { text: '<body>', correct: true },
+            { text: '<footer>', correct: false },
+            { text: '<!DOCTYPE>', correct: false },
+        ] 
+    
+    },
+
+    {
+        question:'the css link goes in the ____',
+        answers: [
+            { text: '<header>', correct: true },
+            { text: '<body>', correct: false },
+            { text: '<footer>', correct: false },
+            { text: 'css files are linked inately', correct: false },
+        ] 
+    
+    },
+
+    {
+        question:'stack overflow is a ______',
+        answers: [
+            { text: 'game', correct: false },
+            { text: 'blessing', correct: true },
+            { text: 'error', correct: false },
+            { text: 'virus', correct: false },
+        ] 
+    
+    }
+]
